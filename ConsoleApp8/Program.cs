8c94ca561bf03aadd1b8273ee268c169e28b41e6
admin <admin@DESKTOP-NH7NBB7>
2022-10-03 20:01:36 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,23 @@
+﻿
+while (true)
+{
+    Random tan = new Random();
+    int a = tan.Next(0, 100);
+    int b;
+    b = Convert.ToInt32(Console.ReadLine());
+    if (b != a)
+    {
+        Console.WriteLine("Вы ввели неверное число, введите другое");
+       
+    }
+    else
+    {
+        Console.WriteLine("Победа");
+        break;
+    }
+
+
+
+
+}
+

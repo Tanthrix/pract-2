ef2fa047854365aef64b1b300218985968089e7e
Tanthrix <112683365+Tanthrix@users.noreply.github.com>
2022-11-03 09:34:20 +0300

    Update Program.cs

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -1,23 +1,63 @@
 ﻿
 while (true)
 {
-    Random tan = new Random();
-    int a = tan.Next(0, 100);
-    int b;
-    b = Convert.ToInt32(Console.ReadLine());
-    if (b != a)
+    Console.WriteLine("Выберете игру: ");
+    Console.WriteLine("1) Угадай число");
+    Console.WriteLine("2) Таблица умножения");
+    Console.WriteLine("3) Вывод делителей числа");
+    Console.WriteLine("4) Выход");
+    int a = Convert.ToInt32(Console.ReadLine());
+    switch (a)
     {
-        Console.WriteLine("Вы ввели неверное число, введите другое");
-       
+        case 1:
+            Random random = new Random();
+            int b = random.Next(0, 100);
+            string input;
+            Console.WriteLine("Угадай число от 1 до 100");
+            do
+            {
+                input = Console.ReadLine();
+                if (int.Parse(input) > b)
+                    Console.WriteLine("Ваше число больше");
+                if (int.Parse(input) < b)
+                    Console.WriteLine("Ваше число меньше");
+
+            }
+            while (int.Parse(input) != b);
+            Console.WriteLine("Вы победили");
+            break;
+
+        case 2:
+            int[,] table = new int[10, 10];
+            for (int i = 1; i < 10; ++i)
+            {
+                for (int j = 1; j < 10; ++j)
+                {
+                    table[i, j] = i * j;
+                    Console.Write("  ", table[i, j]);
+                }
+                Console.WriteLine();
+                
+            }
+            break;
+
+        case 3:
+
+            Console.WriteLine("Поиск делителей числа");
+            string z = Console.ReadLine().ToLower();
+            int number = Convert.ToInt32(z);
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+            break;
+
+        case 4:
+            Environment.Exit(0);
+            break;
+
     }
-    else
-    {
-        Console.WriteLine("Победа");
-        break;
-    }
-
-
-
-
 }
-
